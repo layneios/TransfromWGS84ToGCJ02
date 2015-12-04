10afecb5b2b4c1cd8cdf98361467349a7813167c
冷漠叻荭颜 <lengmohongyan@hotmail.com>
2015-12-04 14:20:46 +0800

    init commit

---
+++ WGS84TOGCJ02.h
@@ -0,0 +1,19 @@
+//
+//  WGS84TOGCJ02.h
+//  EdaixiHousekeeper
+//
+//  Created by lengmolehongyan on 15/12/4.
+//  Copyright © 2015年 lengmolehongyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface WGS84TOGCJ02 : NSObject
+
+/** 判断是否已经超出中国范围 */
++ (BOOL)isLocationOutOfChina:(CLLocationCoordinate2D)location;
+
+/** 将经纬度转成 GCJ02 */
++ (CLLocationCoordinate2D)transformFromWGSToGCJ:(CLLocationCoordinate2D)wgsLoc;
+@end
